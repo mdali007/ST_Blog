e4ba25e42789c383583082c66b135a1b5a57ed65
Muhammed Shameer Ali <92969341+mdali007@users.noreply.github.com>
2023-12-16 07:16:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,21 +13,6 @@ import os
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
-
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
@@ -71,10 +56,9 @@ class BlogPost(db.Model):
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250))
     # Parent relationship to the comments
     comments = relationship("Comment", back_populates="parent_post")
-    # likes for post
     likes = db.Column(db.Integer, default=0)
     likers = relationship("Like", back_populates="post")
 
